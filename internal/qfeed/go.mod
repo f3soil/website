677aec582aa510b8f12c11f7baccec143267b0bd
Nathaniel Nutter <iam@nnutter.com>
2024-03-03 16:08:03 -0500

    Add QFeed

---
+++ internal/qfeed/go.mod
@@ -0,0 +1,5 @@
+module github.com/f3soil/website/internal/qfeed
+
+go 1.22.0
+
+require github.com/gorilla/feeds v1.1.2
---
+++ internal/qfeed/main.go
@@ -0,0 +1,320 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gorilla/feeds"
+)
+
+func main() {
+	feed := GenerateFeed()
+	rss, err := feed.ToRss()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "ERROR: %s", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(rss)
+}
+
+func GenerateFeed() feeds.Feed {
+	feed := feeds.Feed{
+		Title: "F3 QSource",
+	}
+
+	startDate := time.Date(time.Now().Year(), 1, 1, 0, 0, 0, 0, time.UTC)
+	elapsed := time.Since(startDate)
+	weekNumber := int(elapsed/(7*24*time.Hour)) + 1
+	qPointsIndex := weekNumber + 1
+	if weekNumber < 1 {
+		weekNumber = 1
+	}
+	if weekNumber >= len(qPoints) {
+		weekNumber = len(qPoints)
+	}
+	for _, q := range qPoints[0:qPointsIndex] {
+		feed.Items = append(feed.Items, &feeds.Item{
+			Title: q.Title,
+			Link: &feeds.Link{
+				Href:   q.Link,
+				Rel:    "",
+				Type:   "",
+				Length: "",
+			},
+		})
+	}
+	return feed
+}
+
+type QPoint struct {
+	Title string
+	Link  string
+}
+
+var qPoints = []QPoint{
+	{
+		Title: "QSource The F3 Manual of Virtuous Leadership",
+		Link:  "https://f3nation.com/q/",
+	},
+	{
+		Title: "Disruption (F1)",
+		Link:  "https://f3nation.com/q/disruption/",
+	},
+	{
+		Title: "Language (F2)",
+		Link:  "https://f3nation.com/q/language/",
+	},
+	{
+		Title: "Group (F3)",
+		Link:  "https://f3nation.com/q/group/",
+	},
+	{
+		Title: "LDP (F4)",
+		Link:  "https://f3nation.com/q/ldp/",
+	},
+	{
+		Title: "Preparedness (F5)",
+		Link:  "https://f3nation.com/q/preparedness/",
+	},
+	{
+		Title: "Get Right (Q1)",
+		Link:  "https://f3nation.com/q/get-right/",
+	},
+	{
+		Title: "DRP (Q1.1)",
+		Link:  "https://f3nation.com/q/drp/",
+	},
+	{
+		Title: "King (Q1.2)",
+		Link:  "https://f3nation.com/q/king/",
+	},
+	{
+		Title: "Queen (Q1.3)",
+		Link:  "https://f3nation.com/q/queen/",
+	},
+	{
+		Title: "Jester (Q1.4)",
+		Link:  "https://f3nation.com/q/jester/",
+	},
+	{
+		Title: "M (Q1.5)",
+		Link:  "https://f3nation.com/q/m/",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+	{
+		Title: "",
+		Link:  "",
+	},
+}
